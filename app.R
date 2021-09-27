8a508bd51d96dc588b5bac70d19a8a9de93820e9
Patrick <patrick.hiemsch@gmail.com>
2021-09-27 17:03:55 +0200

    added elements, plot needs fix

--- app.R
+++ app.R
@@ -1,49 +1,59 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(apikolada)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
+all_kpis_df <- get_all_kpis()
+all_kpis <- unique(stringr::str_sort(all_kpis_df[["member_title"]]))
+
+all_cities <- get_all_cities()
+
+
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+  titlePanel("KPI visualizations for API-requests to kolada.se"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+  sidebarLayout(
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    sidebarPanel(
+      selectInput("kpi", "Choose Key Performance Indicator (KPI):", all_kpis),
+      selectInput("city", "Choose city:", all_cities, selected = "Linköping"),
+      sliderInput("years", "Choose years range for KPIs:", value = c(2017, 2021), min = 2000, max = 2021, sep = ""),
+      tableOutput("data_tab")
+    ),
+
+    mainPanel(
+      plotOutput("kpi_plot")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+server <- function(input, output, session) {
+
+  # reactive programming, gets refreshed when input changes, globlaly available
+  kpi_code <- reactive(all_kpis_df[all_kpis_df$member_title == input$kpi, ][["member_id"]])
+  data <- reactive(get_infos(kpis = c(kpi_code()), cities = c(input$city), years = seq(input$years[1], input$years[2], 1))[c("kpi", "period", "value")])
+
+
+
+  output$data_tab <- renderTable({
+    data()
+  })
+
+  output$kpi_plot <- renderPlot({
+    ggplot(data(), aes(period, value)) + geom_point()
+  }, res = 96)
+
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
+
+
+#kpi_code <- all_kpis_df[all_kpis_df$member_title == all_kpis[1], ][["member_id"]]
+
+#res <- get_infos(cities=c("Linköping"), kpis = c(kpi_code), years = c(2009, 2021))
+
+#plot(x=res$period, y=res$value)
+
+#kpi_code seq(2012, 2021, 1)
